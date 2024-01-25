a13c66d58ef19d48f13835c4ee3dd62798a0adef
tritone <linyonggui@xiaohongshu.com>
2024-01-25 23:12:03 +0800

    引入自带数据

--- chat2rs-server/chat2rs-server-start/src/main/java/com/research/chat/rsstart/Application.java
+++ chat2rs-server/chat2rs-server-start/src/main/java/com/research/chat/rsstart/Application.java
@@ -10,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+
+        ResourceInit.init();
+
         SpringApplication.run(Application.class, args);
     }
 
